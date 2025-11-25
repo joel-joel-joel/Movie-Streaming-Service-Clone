54d524cdebef179bd6e2c180d2a504d7387bb3f6
joel-joel-joel <leojongenli@gmail.com>
2025-11-25 16:47:07 +0000

    Basic tab alterations, fixed dependency issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,9 @@ import { Stack } from "expo-router";
 import "./globals.css"
 
 export default function RootLayout() {
-  return <Stack >
-      // Hide tab bar on all screens
-      <Stack.Screen name="(tabs)"
-                    options={{headerShown: false}}/>
+  return <Stack>
+      /* Hide tab bar on all screens */
+      <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
 
       // Hide header on movie details screen/
       <Stack.Screen name="movie/[id]"
